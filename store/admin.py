d874361e533c87b1447a6aa91701d24b7f33be1f
prederyo <ifkaraca14@gmail.com>
2025-12-27 00:43:13 +0300

    yönetim panaeli düzenlendi

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category,Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name","slug",)
+    search_fields = ["name"]
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name","category","price","discount_rate","stock","is_active","vendor",)
+    list_filter = ("category",)
+    search_fields = ["name"] 
+
